6add59f8aa094c82456c0ce090a94645a1288550
Aaron <aaron.duane24601@gmail.com>
2023-02-16 00:31:04 +0100

    adding code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { readString } from 'react-papaparse';
 import './App.css';
 
-function App() {
+// load static data
+import paymentsCSV from './data/payments.csv';
+import usersCSV from './data/users.csv';
+
+const App = () => {
+
+  const [payments, setPayments] = useState(0);
+  const [users, setUsers] = useState(0);
+
+  const paymentsConfig = {
+    download: true,
+    complete: results => {
+        const payments = {data: results.data};
+        setPayments(payments);
+    },
+    error: (error, file) => {
+      console.log('Error while parsing:', error, file);
+    }
+  };
+
+  const usersConfig = {
+    download: true,
+    complete: results => {
+        const users = {data: results.data};
+        setUsers(users);
+    },
+    error: (error, file) => {
+      console.log('Error while parsing:', error, file);
+    }
+  };
+  
+  useEffect(() => {
+    if (payments === 0) {
+      readString(paymentsCSV, paymentsConfig);
+    }
+    if (users === 0) {
+      readString(usersCSV, usersConfig);
+    }
+  }, []);
+  
+  useEffect(() => { 
+    if (payments.data != undefined) {
+
+      for (let i = 0; i < payments.data.length; i++) {
+        const item = payments.data[i];
+        if (i === 0 || item[0] === "i9tlVXuPmPUBgIUMYQkMET1gT0wyQqlqLlQU0q3+1UU=") {
+          console.log(item);
+        }
+      }
+
+    }
+  }, [payments]);
+
+  useEffect(() => { 
+    if (users.data != undefined) {
+
+      for (let i = 0; i < users.data.length; i++) {
+        const item = users.data[i];
+        if (i === 0 || item[0] === "i9tlVXuPmPUBgIUMYQkMET1gT0wyQqlqLlQU0q3+1UU=") {
+          console.log(item);
+        }
+      }
+      
+    }
+  }, [users]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Users: {users.data === undefined ? "Reading..." : users.data.length + " rows"}
+      <br />
+      Payments: {payments.data === undefined ? "Reading..." : payments.data.length + " rows"}
     </div>
   );
 }
